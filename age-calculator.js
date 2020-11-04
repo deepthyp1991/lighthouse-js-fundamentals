f5978c8fb812c1a38fdbe8501984eeb90be6e7b0
deepthyp1991 <deepthyp1991@gmail.com>
2020-11-04 23:13:06 +0000

    Practicing branch

--- age-calculator.js
+++ age-calculator.js
@@ -8,14 +8,3 @@ function ageCalculator(name, yearOfBirth, currentYear){
 
 console.log(ageCalculator("Suzie", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
-
-// function ageCalculator(name, yearOfBirth, currentYear){
-//   let age;
-
-//   return ( age = currentYear - yearOfBirth ? name + ' is' + ' ' + age + ' years old' : age)
-    
-
-// }
-
-// console.log(ageCalculator("Miranda", 1983, 2015));
-// console.log(ageCalculator("Ferdinand", 1988, 2015));
